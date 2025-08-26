565e48dc1fd9f6175066ca15506e506fd0b9491b
samuelzzx7 <samuelbarreto203040@gmail.com>
2025-08-26 19:09:13 -0300

    pagina de créditos
    adição da pagina de creditos

--- capitao.js
+++ capitao.js
@@ -1,6 +1,9 @@
 const fim = document.getElementById("fim");
 const video = document.getElementById("video");
 const fechar = document.getElementById("fechar");
+const meuVideo = document.getElementById("meuVideo");
+
+meuVideo.pause();
 
 fim.addEventListener("click", () => {
     video.style.display = "block";
@@ -11,3 +14,4 @@ fechar.addEventListener("click", () => {
     video.style.display = "none";
     fim.style.display = "block";
 })
+
